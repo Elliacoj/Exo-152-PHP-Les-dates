dc1e1016980d0d1e7215bdee1bafe53bb135ec2e
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-01-27 13:12:27 +0100

    Modif

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
 // Premiere ligne
+echo date("d-m-y") . "<br>";
 
+$date = new DateTime('2018-12-10 11:35:07');
 
+echo $date->format('Y.m.d') . "<br>";
 
-
-
+echo $date->format('h:i:s') . "<br>";
 
